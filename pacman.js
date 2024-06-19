d236a95a521ead4c8257938bdfad16a0fa603ee7
Noah do Rego <ndore051@uottawa.ca>
2024-06-19 12:43:29 -0400

    Added comments to JS for documentation

--- pacman.js
+++ pacman.js
@@ -1,31 +1,39 @@
+// global variable declarations
 var score = 0, pacLoc = 0, fruitLoc = 0, ghostLoc = -1;
 var board = [];
 var player = "😀", ghost = "👻";
 var ateGhost = true, gameEnded = false;
 
+// function for creating and initializing game board
 function createGame(n) {
     board = new Array(n);
     board.fill("◦");
 
-    // Initialize Pacman in middle
+    // initialize Pacman in middle
     pacLoc = Math.floor(n / 2);
     board[pacLoc] = player;
 
-    // Randomize fruit on board
+    // randomize fruit on board
     fruitLoc = fruitSpawnLocation(n, pacLoc);
     board[fruitLoc] = "🍒";
 
+    // set HTML for board
     document.getElementById("board").innerHTML = board.join(" ");
     document.getElementById("score").innerHTML = score;
 }
 
+// returns a random available index for spawning fruit
 function fruitSpawnLocation(length, pacLoc) {
     var num = Math.floor(Math.random() * length);
     return (num === pacLoc) ? num + 1 : num;
 }
 
+// moves pacman in a direction via left/right keys
 function movePac(direction) {
+    // set last position to empty
     board[pacLoc] = "&nbsp;";
+
+    // handle left/right presses
     if (direction == 'right') {
         pacLoc = (pacLoc + 1) % (board.length);
         checkLocation(pacLoc);
@@ -35,6 +43,7 @@ function movePac(direction) {
         checkLocation(pacLoc);
     }
 
+    // check for pellets, fill board if none exist
     if (!board.includes("◦")) {
         board = board.toString().replaceAll("&nbsp;", "◦").split(",");
         fruitLoc = fruitSpawnLocation(board.length, pacLoc);
@@ -43,18 +52,22 @@ function movePac(direction) {
     }
 }
 
+// checks the location that pacman has moved to and changes variables accordingly
 function checkLocation(loc) {
     if (board[loc] != " ") {
         if (board[loc] == "◦") {
+            // increment score for pellets
             score += 1;
             document.getElementById("score").innerHTML = score;
         }
         else if (board[loc] == "🍒") {
+            // turn ghost to scared if fruit is eaten
             ghost = "😱"
             board[ghostLoc] = ghost;
             document.getElementById("board").innerHTML = board.join(" ");
         }
         else if (board[loc] == "😱") {
+            // increment score by 5 if ghost is eaten, respawn ghost after 2 seconds
             document.getElementById("board").innerHTML = board.join(" ");
             score += 5;
             document.getElementById("score").innerHTML = score;
@@ -62,14 +75,17 @@ function checkLocation(loc) {
             setTimeout(spawnGhost, 2000);
         }
         else if (board[loc] == "👻") {
+            // end game if ghost is hit
             endGame();
         }
     }
 
+    // update player location
     board[pacLoc] = player;
     document.getElementById("board").innerHTML = board.join(" ");
 }
 
+// spawn ghost on opposite side of Pacman
 function spawnGhost() {
     if (ateGhost) {
         ghost = "👻";
@@ -87,6 +103,7 @@ function spawnGhost() {
     }
 }
 
+// move ghost towards Pacman if normal, move away from Pacman if scared
 function moveGhost() {
 
     if (gameEnded) return;
@@ -113,7 +130,6 @@ function moveGhost() {
 
     if (ghostLoc == pacLoc) {
         if (ghost == "👻") {
-            console.log("being called here 1!")
             checkLocation(pacLoc);
         } else if (ghost == "😱") {
             document.getElementById("board").innerHTML = board.join(" ");
@@ -125,6 +141,7 @@ function moveGhost() {
         return;
     }
 
+    // move ghost every 1 second
     setTimeout(() => {
         if (!gameEnded && !ateGhost) {
             moveGhost();
@@ -132,6 +149,7 @@ function moveGhost() {
     }, 1000);
 }
 
+// ends game by setting flag, changing player icon, updating HTML
 function endGame() {
     gameEnded = true;
     player = "💀";
@@ -141,7 +159,7 @@ function endGame() {
     return;
 }
 
-// Detect key presses
+// detect key presses
 document.onkeyup = function (e) {
     if (!gameEnded && player != "💀") {
         switch (e.key) {
@@ -155,5 +173,6 @@ document.onkeyup = function (e) {
     }
 };
 
+// create board and spawn ghost after 2 seconds
 createGame(10);
 setTimeout(spawnGhost, 2000);
